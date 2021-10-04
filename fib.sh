c36bf877618b26b62a8dbcb204dfcbd373681e41
Jirka Kremser <jiri.kremser@gmail.com>
2021-10-04 15:33:51 +0200

    init

---
+++ fib.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -l
+
+[[ $# != 1 ]] && echo "Usage: $0 <n>" && exit 1
+! [[ $1 =~ ^[0-9]+$ ]] && echo "$1 is not a number" && exit 2
+
+
+N=$1
+a=0
+b=1 
+ 
+for (( i=0; i<N; i++ )); do
+    fn=$((a + b))
+    a=$b
+    b=$fn
+done
+
+echo Fib number w/ ord $1 is $a
